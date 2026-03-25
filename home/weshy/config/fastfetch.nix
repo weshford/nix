5ca30b3b840bbb2061b60c394bf8388010ff2644
weshford <95880628+weshford@users.noreply.github.com>
2026-03-26 00:06:55 +0100

    config for weathr fastfetch and kitty

---
+++ home/weshy/config/fastfetch.nix
@@ -0,0 +1,9 @@
+{ ... }:
+
+{
+  programs.fastfetch = {
+    settings = {
+      logo = "auto";
+    };
+  };
+}
---
+++ home/weshy/config/kitty.nix
@@ -0,0 +1,25 @@
+{ ... }:
+
+{
+  programs.kitty = {
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 11.5;
+    };
+    settings = {
+      background_opacity = "0.90";
+      dynamic_background_opacity = "yes";
+      window_padding_width = 10;
+      cursor_shape = "beam";
+      cursor_blink_interval = "0.5";
+      enable_audio_bell = "no";
+      confirm_os_window_close = 0;
+      foreground = "#EDEDED";
+      background = "#1D1B25";
+      selection_foreground = "#F5F5F5";
+      selection_background = "#6E5191";
+      active_border_color = "#6E5191";
+      inactive_border_color = "#3D3352";
+    };
+  };
+}
