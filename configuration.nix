b0799f1b0cc42ed48a4fa8cd9be7c71dffb689f3
yungztrunks <95880628+yungztrunks@users.noreply.github.com>
2026-03-25 15:18:43 +0100

    fixed linking

--- configuration.nix
+++ configuration.nix
@@ -7,8 +7,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./git.nix
+      ./home/weshy/hardware.nix
+      ./modules/git.nix
       ./modules/apps.nix
       ./modules/cli.nix
       ./modules/development.nix
