dcff2ce30b4006bce2bd9c7cc795f2b8fbf0a290
weshford <95880628+weshford@users.noreply.github.com>
2026-03-28 12:07:19 +0100

    multi hosts & multi user setup (as if I'll ever need them)

--- home/default.nix
+++ home/default.nix
@@ -1,8 +1,15 @@
-{ hostName, userName }:
+{ hostName, userNames ? [ ] }:
 
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.backupFileExtension = "hm-backup";
-  home-manager.users.${userName} = import ./${hostName}/default.nix;
+  home-manager.users = builtins.listToAttrs (
+    map
+      (userName: {
+        name = userName;
+        value = import ./${hostName}/default.nix;
+      })
+      userNames
+  );
 }
