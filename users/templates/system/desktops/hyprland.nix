a0d81bf2367b44075199ca3d4c8777b474299f0e
weshford <95880628+weshford@users.noreply.github.com>
2026-04-11 00:48:07 +0200

    desktop template

--- users/templates/system/desktops/hyprland.nix
+++ users/templates/system/desktops/hyprland.nix
@@ -1,5 +1,21 @@
 { lib, osConfig, ... }:
 
 lib.mkIf (osConfig.programs.hyprland.enable or false) {
-  # Hyprland-specific Home Manager settings.
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      exec-once = [
+        "noctalia-shell"
+      ];
+
+      "$mod" = "SUPER";
+      "$terminal" = "kitty";
+      "$ipc" = "noctalia-shell ipc call";
+
+      bind = [
+        "$mod, Return, exec, $terminal"
+        "$mod, D, exec, $ipc launcher command"
+      ];
+    };
+  };
 }
--- users/templates/system/desktops/kde.nix
+++ users/templates/system/desktops/kde.nix
@@ -1,5 +1,16 @@
 { lib, osConfig, ... }:
 
 lib.mkIf (osConfig.services.desktopManager.plasma6.enable or false) {
-  # KDE Plasma-specific Home Manager settings.
+  qt = {
+    enable = true;
+    platformTheme.name = "kde";
+  };
+
+  home.file.".config/kdeglobals".text = ''
+    [General]
+    ColorScheme=BreezeDark
+
+    [KDE]
+    SingleClick=false
+  '';
 }
