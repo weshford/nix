0ff3d840d0147e83f451bc7572d340f0d34d8913
yungztrunks <95880628+yungztrunks@users.noreply.github.com>
2026-04-10 23:00:44 +0200

    des au

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,5 +1,12 @@
 { ... }:
 
 {
-  # Shared host module skeleton.
+  # Shared host defaults.
+  networking.networkmanager.enable = true;
+
+  my.modules.desktopSpecialisations = {
+    buildHyprland = true;
+    buildNiri = true;
+    buildKde = true;
+  };
 }
