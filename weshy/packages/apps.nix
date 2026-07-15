61353eeabf5998f11ebc6ba05d49970157cbea47
weshford <95880628+weshford@users.noreply.github.com>
2026-07-15 20:06:12 +0200

    junge kp

--- weshy/packages/apps.nix
+++ weshy/packages/apps.nix
@@ -16,23 +16,23 @@
     libsixel
   ];
 
-  home.file.".local/bin/discord" = {
-    executable = true;
-    text = ''
-      #!${pkgs.bash}/bin/bash
-      exec env GTK_USE_PORTAL=1 XDG_CURRENT_DESKTOP=Hyprland XDG_SESSION_DESKTOP=Hyprland NIXOS_OZONE_WL=1 ELECTRON_OZONE_PLATFORM_HINT=auto ${pkgs.discord}/bin/Discord --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
-    '';
-  };
+  # home.file.".local/bin/discord" = {
+  #   executable = true;
+  #   text = ''
+  #     #!${pkgs.bash}/bin/bash
+  #     exec env GTK_USE_PORTAL=1 XDG_CURRENT_DESKTOP=Hyprland XDG_SESSION_DESKTOP=Hyprland NIXOS_OZONE_WL=1 ELECTRON_OZONE_PLATFORM_HINT=auto ${pkgs.discord}/bin/Discord --enable-features=UseOzonePlatform --ozone-platform=wayland "$@"
+  #   '';
+  # };
 
-  home.file.".local/share/applications/discord.desktop".text = ''
-    [Desktop Entry]
-    Type=Application
-    Name=Discord
-    Exec=${config.home.homeDirectory}/.local/bin/discord %U
-    Icon=discord
-    Terminal=false
-    Categories=Network;Chat;InstantMessaging;
-    MimeType=x-scheme-handler/discord;x-scheme-handler/discord-https;
-    StartupNotify=true
-  '';
+  # home.file.".local/share/applications/discord.desktop".text = ''
+  #   [Desktop Entry]
+  #   Type=Application
+  #   Name=Discord
+  #   Exec=${config.home.homeDirectory}/.local/bin/discord %U
+  #   Icon=discord
+  #   Terminal=false
+  #   Categories=Network;Chat;InstantMessaging;
+  #   MimeType=x-scheme-handler/discord;x-scheme-handler/discord-https;
+  #   StartupNotify=true
+  # '';
 }
