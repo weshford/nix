e3264464b4ab31418fe1105ef6c926aa9045be99
weshford <95880628+weshford@users.noreply.github.com>
2026-08-18 16:30:34 +0200

    behinderte dings noc

--- configuration.nix
+++ configuration.nix
@@ -24,10 +24,12 @@
     substituters = [
       "https://cache.nixos.org/"
       "https://kopuz.cachix.org/"
+      "https://weshford.cachix.org/"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "kopuz.cachix.org-1:J2X3AnAYhKTJW5S3aCLoA1ckonQXVNZMQvhZA0YAufw="
+      "weshford.cachix.org-1:J2X3AnAYhKTJW5S3aCLoA1ckonQXVNZMQvhZA0YAufw="
     ];
 
   };
